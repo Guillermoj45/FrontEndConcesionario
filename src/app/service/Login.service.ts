a74bb2fa50383ee23bca7834c88eccb418892eba
guill <guillermoremesa@gmail.com>
2024-12-14 20:37:57 +0100

    Serviocio de inicio de sesion hecho

---
+++ src/app/service/Login.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpContext} from "@angular/common/http";
+import {SkipAuth} from "./Interceptor";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http:HttpClient) { }
+
+  login(username:string, password:string):Observable<string> {
+    const params = {
+      username: username,
+      password: password
+    }
+    return this.http.post<string>("/api/user/login", params, {context: new HttpContext().set(SkipAuth, true), responseType: 'text' as 'json'});
+  }
+}
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map, Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private tokenKey = 'api_token';
+
+  constructor(private http: HttpClient) { }
+
+  setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  removeToken(): void {
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  // isAuthenticated(): Observable<Boolean> {
+  //   return this.http.get<AuthVerifyResponse>('/api/auth/verify-token').pipe(
+  //     map((response) => response.valid),
+  //     catchError(() => {
+  //       this.removeToken();
+  //       return [false];
+  //     })
+  //   );
+  // }
+
+}
