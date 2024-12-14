587853ca92215e6b4e37c264bdea24c4d5e09013
guill <guillermoremesa@gmail.com>
2024-12-14 20:37:34 +0100

    Pagina de inicio de sesion hecha

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonInput,
+  IonLabel,
+  IonItem,
+  IonButton
+} from '@ionic/angular/standalone';
+import {FormsModule} from "@angular/forms";
+import {LoginService} from "../service/Login.service";
+import {AuthService} from "../service/auth.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, IonInput, IonLabel, IonItem, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  username: string = "";
+  password: string = "";
+  constructor(private loginService:LoginService, private authService: AuthService) {}
+
+  login() {
+    this.loginService.login(this.username, this.password).subscribe((response) => {
+      console.log(response);
+      this.authService.setToken(response);
+    });
+  }
 }
